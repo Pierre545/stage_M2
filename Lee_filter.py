13cf0cba0f2228f16b926db9dc0de6df24e04474
Pierre545 <57266944+Pierre545@users.noreply.github.com>
2021-06-08 10:13:53 +0200

    Add files via upload
    Lee filter to remove Speckle noise

---
+++ Lee_filter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 10:12:27 2021
+
+@author: pierre
+"""
+
+
+from scipy.ndimage.filters import uniform_filter
+from scipy.ndimage.measurements import variance
+
+def lee_filter(img, size):
+    img_mean = uniform_filter(img, (size, size))
+    img_sqr_mean = uniform_filter(img**2, (size, size))
+    img_variance = img_sqr_mean - img_mean**2
+
+    overall_variance = variance(img)
+
+    img_weights = img_variance / (img_variance + overall_variance)
+    img_output = img_mean + img_weights * (img - img_mean)
+    return img_output
